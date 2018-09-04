bc099d183423e70e84abee2bb11a941d66c0d87d
D4rk-h7ml <42938253+D4rk-h7ml@users.noreply.github.com>
2018-09-04 03:23:20 +0000

    Update logger.php

--- logger.php
+++ logger.php
@@ -1,45 +1,21 @@
 <?php
-
 /*
-
 PHP Simple Logger By D4rk h7ml
-
 Love to Coding,Wish to Spreading
-
 */
 
+
 $file = fopen("ips.txt","w");
-
 $s = "IP::";
-
 fwrite($file,$s);
-
 $ip = $_SERVER["REMOTE_ADDR"];
-
 fwrite($file,$ip);
-
 $i ="\n\n\nOpen Port::";
-
 fwrite($file,$i);
-
 $d = $_SERVER['REMOTE_PORT'];
-
 fwrite($file,$d);
-
-$ni ="\n\n\nUname::";
-
-fwrite($file,$ni);
-
-$n = php_uname();
-
-fwrite($file,$n);
-
-$os = "\n\n\nBrowser::";
-
+$os = "\n\n\nBrowser, Os, Device::";
 fwrite($file,$os);
-
 $b = $_SERVER['HTTP_USER_AGENT'];
-
 fwrite($file,$b);
-
 ?>
