948c6bcb2474267e6f7803caf6da611af1156397
Md Saikat <42938253+0xh7ml@users.noreply.github.com>
2020-11-16 19:09:49 +0600

    Update logger.php

--- logger.php
+++ logger.php
@@ -5,17 +5,9 @@ Love to Coding,Wish to Spreading
 */
 
 
-$file = fopen("ips.txt","w");
 $s = "IP::";
-fwrite($file,$s);
 $ip = $_SERVER["REMOTE_ADDR"];
-fwrite($file,$ip);
-$i ="\n\n\nOpen Port::";
-fwrite($file,$i);
-$d = $_SERVER['REMOTE_PORT'];
-fwrite($file,$d);
 $os = "\n\n\nBrowser, Os, Device::";
-fwrite($file,$os);
 $b = $_SERVER['HTTP_USER_AGENT'];
-fwrite($file,$b);
+mail("someone@gmail.com","Log Info",$s.$ip.$os.$b);
 ?>
